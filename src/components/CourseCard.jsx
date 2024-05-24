9725d94626e1b6e705832d37bd60bcd7bc9bebe2
Rishik Sahu <rishikkr@hotmail.com>
2024-05-24 12:13:37 +0000

    updated links in cards

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,69 +1,109 @@
-import React from 'react'
-import { FlexBox } from '../components/reusables/AllContainers'
-import { Box, Button, Typography } from '@mui/material'
+import React from "react";
+import { FlexBox } from "../components/reusables/AllContainers";
+import { Box, Button, Typography } from "@mui/material";
+import { useContext } from "react";
+import { LanguageContext } from "../context/langContext";
 const CourseCard = ({ data, idx }) => {
+  const { language } = useContext(LanguageContext);
+  const redirectLang = language === "HINDI" ? "hi" : "en";
 
   const durationStr = () => {
-    const hh = Math.floor(data.courseHours)
-    const mm = Math.round((data.courseHours - hh) * 60)
-    const hhStr = hh > 1 ? `${hh} hours ` : hh < 1 ? '' : `${hh} hour `
-    const mmStr = mm > 1 ? `${mm} minutes ` : mm < 1 ? '' : `${mm} minute `
-    return hhStr + mmStr
-  }
+    const hh = Math.floor(data.courseHours);
+    const mm = Math.round((data.courseHours - hh) * 60);
+    const hhStr = hh > 1 ? `${hh} hours ` : hh < 1 ? "" : `${hh} hour `;
+    const mmStr = mm > 1 ? `${mm} minutes ` : mm < 1 ? "" : `${mm} minute `;
+    return hhStr + mmStr;
+  };
+
   return (
-    <FlexBox column sx={{
-      justifyContent: 'flex-start',
-      alignItems: 'flex-start',
-      padding: '1rem',
-      gap: '0.5rem',
-      cursor: 'pointer',
-      borderBottom: '1px solid var(--text-4)',
-      ":hover": {
-        borderRadius: '0.5rem',
-        background: 'var(--card-hover-bg)',
-        boxShadow: '0 0 2px rgba(0,0,0,0.2)',
-        border: 'none',
-      }
-    }} >
-
-      <Box sx={{
-        backgroundColor: 'var(--color-bg-3)',
-        color: 'white',
-        borderRadius: '0.25rem',
-        padding: '0.125rem 0.5rem',
-        position: 'relative',
-        fontSize: '0.75rem',
-        mb: '0.5rem',
-      }}>
-        भाग {idx + 1}
-      </Box>
-      <Typography sx={{fontSize: '0.875rem'}} fontWeight={600}>{data.title}</Typography>
-      <Typography sx={{fontSize: '0.875rem'}} >{data.subtitle}</Typography>
-      <Typography sx={{fontSize: '0.875rem'}}>{durationStr()}</Typography>
-      <Typography sx={{fontSize: '0.875rem'}}>Contribution: ₹{data.amount} <s>₹{data.originalAmount}</s></Typography>
-      <Typography sx={{
-        fontSize: '0.75rem',
-        backgroundColor: 'rgb(248 250 252)'
-      }}>{data.language.slice(0, 1).toUpperCase() + data.language.slice(1)}</Typography>
-
-      <FlexBox sx={{
-        justifyContent: 'flex-start',
-      }}>
-        <Typography sx={{color: 'var(--color-secondary)', fontSize: '0.75rem',
+    <a
+      target="_blank"
+      href={`https://acharyaprashant.org/${redirectLang}/video-modules/module/${data?.id}`}
+      style={{ textDecoration: "inherit", color: "inherit" }}
+    >
+      <FlexBox
+        column
+        sx={{
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          padding: "1rem",
+          gap: "0.5rem",
+          cursor: "pointer",
+          borderBottom: "1px solid var(--text-4)",
           ":hover": {
-            color: 'var(--color-primary)'
-          }
-         }}>ADD TO CART</Typography>
-        <Typography sx={{color: 'var(--color-bg-3)', fontSize: '0.75rem'}}>|</Typography>
-        <Typography sx={{color: 'var(--color-secondary)' , fontSize: '0.75rem',
-          ":hover": {
-            color: 'var(--color-primary)'
-          }
-        }}>ENROLL NOW</Typography>
+            borderRadius: "0.5rem",
+            background: "var(--card-hover-bg)",
+            boxShadow: "0 0 2px rgba(0,0,0,0.2)",
+            border: "none",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            backgroundColor: "var(--color-bg-3)",
+            color: "white",
+            borderRadius: "0.25rem",
+            padding: "0.125rem 0.5rem",
+            position: "relative",
+            fontSize: "0.75rem",
+            mb: "0.5rem",
+          }}
+        >
+          भाग {idx + 1}
+        </Box>
+        <Typography sx={{ fontSize: "0.875rem" }} fontWeight={600}>
+          {data.title}
+        </Typography>
+        <Typography sx={{ fontSize: "0.875rem" }}>{data.subtitle}</Typography>
+        <Typography sx={{ fontSize: "0.875rem" }}>{durationStr()}</Typography>
+        <Typography sx={{ fontSize: "0.875rem" }}>
+          Contribution: ₹{data.amount} <s>₹{data.originalAmount}</s>
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "0.75rem",
+            backgroundColor: "rgb(248 250 252)",
+            padding: "0.125rem 0.25rem",
+            color: "var(--text-2)",
+          }}
+        >
+          {data.language.slice(0, 1).toUpperCase() + data.language.slice(1)}
+        </Typography>
+
+        <FlexBox
+          sx={{
+            justifyContent: "flex-start",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "var(--color-secondary)",
+              fontSize: "0.75rem",
+              ":hover": {
+                color: "var(--color-primary)",
+              },
+            }}
+          >
+            ADD TO CART
+          </Typography>
+          <Typography sx={{ color: "var(--color-bg-3)", fontSize: "0.75rem" }}>
+            |
+          </Typography>
+          <Typography
+            sx={{
+              color: "var(--color-secondary)",
+              fontSize: "0.75rem",
+              ":hover": {
+                color: "var(--color-primary)",
+              },
+            }}
+          >
+            ENROLL NOW
+          </Typography>
+        </FlexBox>
       </FlexBox>
+    </a>
+  );
+};
 
-    </FlexBox>
-  )
-}
-
-export default CourseCard
+export default CourseCard;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,214 +1,261 @@
-import React, { useState, useContext } from 'react'
-import { FlexBox } from './reusables/AllContainers'
-import { Box, Typography } from '@mui/material'
-import { ArrowDropDown, MenuOutlined, PhoneOutlined } from '@mui/icons-material';
-import { useNavigate } from 'react-router-dom';
-import { LanguageContext } from '../context/langContext';
+import React, { useState, useContext } from "react";
+import { FlexBox } from "./reusables/AllContainers";
+import { Box, Typography } from "@mui/material";
+import {
+  ArrowDropDown,
+  MenuOutlined,
+  PhoneOutlined,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { LanguageContext } from "../context/langContext";
 
 const Header = () => {
+  const { language, setLanguage, langStr } = useContext(LanguageContext);
 
-    const { language, setLanguage, langStr } = useContext(LanguageContext)
-
-    const switchLanguage = () => {
-        if (language === 'HINDI') {
-            setLanguage('ENGLISH')
-        } else {
-            setLanguage('HINDI')
-        }
+  const switchLanguage = () => {
+    if (language === "HINDI") {
+      setLanguage("ENGLISH");
+    } else {
+      setLanguage("HINDI");
     }
+  };
 
-    const navMenuList = [
-        { title: langStr.nav_menu.menu_1, link: '/' },
+  const navMenuList = [
+    { title: langStr.nav_menu.menu_1, link: "/" },
+    {
+      title: "Live Sessions",
+      link: "",
+      subMenu: [
         {
-            title: 'Live Sessions',
-            link: '',
-            subMenu: [
-                {
-                    title: 'Gita Samagam',
-                    link: 'https://acharyaprashant.org/en/gita'
-                },
-                {
-                    title: 'Vedanta: Basics to Classics',
-                    link: 'https://acharyaprashant.org/en/vedanta-basics-to-classics'
-                }]
+          title: "Gita Samagam",
+          link: "https://acharyaprashant.org/en/gita",
         },
         {
-            title: 'Video Series',
-            link: 'https://acharyaprashant.org/en/video-modules',
+          title: "Vedanta: Basics to Classics",
+          link: "https://acharyaprashant.org/en/vedanta-basics-to-classics",
+        },
+      ],
+    },
+    {
+      title: "Video Series",
+      link: "https://acharyaprashant.org/en/video-modules",
+    },
+    {
+      title: "AP Books",
+      link: "https://acharyaprashant.org/en/books",
+    },
+    {
+      title: "AP Articles",
+      link: "https://acharyaprashant.org/en/articles",
+    },
+    {
+      title: "Invite",
+      link: "",
+      subMenu: [
+        {
+          title: "For a talk",
+          link: "https://acharyaprashant.org/en/invite?tIn=0",
         },
         {
-            title: 'AP Books',
-            link: 'https://acharyaprashant.org/en/books',
+          title: "For an Interview",
+          link: "https://acharyaprashant.org/en/invite?tIn=1",
         },
-        {
-            title: 'AP Articles',
-            link: 'https://acharyaprashant.org/en/articles',
-        },
-        {
-            title: 'Invite',
-            link: '',
-            subMenu: [
-                {
-                    title: 'For a talk',
-                    link: 'https://acharyaprashant.org/en/invite?tIn=0'
-                },
-                {
-                    title: 'For an Interview',
-                    link: 'https://acharyaprashant.org/en/invite?tIn=1'
-                }]
-        },
-        {
-            title: 'Donate',
-            link: 'https://acharyaprashant.org/en/contribute'
-        },
+      ],
+    },
+    {
+      title: "Donate",
+      link: "https://acharyaprashant.org/en/contribute",
+    },
+  ];
 
+  const navigate = useNavigate();
 
-    ]
+  const [showSubMenu, setShowSubMenu] = useState(false);
 
-    const navigate = useNavigate()
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+      }}
+    >
+      <FlexBox
+        sx={{
+          height: "3rem",
+          justifyContent: "space-between",
+          backgroundColor: "var(--color-primary)",
+          padding: { md: "1rem 2rem", sm: "1rem", xs: "1rem" },
+        }}
+      >
+        <FlexBox sx={{ width: "fit-content", gap: "2rem" }}>
+          <img
+            style={{
+              height: "2rem",
+              width: "2rem",
+              cursor: "pointer",
+            }}
+            src="https://acharyaprashant.org/images/ic_favicon.png"
+            alt="Acharya Prashant"
+          />
+          <FlexBox
+            sx={{
+              gap: { md: "2rem", sm: "1rem" },
+              display: { md: "flex", sm: "flex", xs: "none" },
+            }}
+          >
+            {navMenuList.map((item, idx) => (
+              <FlexBox
+                onClick={() => {
+                  item?.subMenu
+                    ? setShowSubMenu(true)
+                    : (window.location.href = `${item.link}`);
+                }}
+                sx={{
+                  width: "fit-content",
+                  color: "white",
+                  height: "100%",
+                  gap: 0,
+                  cursor: "pointer",
+                }}
+                key={idx}
+              >
+                <Typography sx={{ fontSize: "0.875rem", textWrap: "nowrap" }}>
+                  {item.title}
+                </Typography>
+                {item?.subMenu ? <ArrowDropDown /> : <></>}
 
-    const [showSubMenu, setShowSubMenu] = useState(false)
+                {item?.subMenu && showSubMenu && <SubMenu />}
+              </FlexBox>
+            ))}
+          </FlexBox>
+        </FlexBox>
 
-    return (
+        <FlexBox sx={{ width: "fit-content", color: "white", gap: ".75rem" }}>
+          <Box
+            sx={{
+              color: "white",
+              fontWeight: "600",
+              border: "2px solid white",
+              borderRadius: "0.25rem",
+              fontSize: "0.8rem",
+              paddingX: "0.5rem",
+              display: { lg: "none", md: "none", sm: "none", xs: "flex" },
+              alignItems: "center",
+            }}
+          >
+            Donate
+          </Box>
+          <FlexBox gap={0} sx={{ cursor: "pointer" }} onClick={switchLanguage}>
+            <Box
+              sx={{
+                color: "white",
+                fontWeight: "600",
+                border: "2px solid white",
+                borderRadius: "0.25rem",
+                fontSize: "0.8rem",
+                paddingX: "0.5rem",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              {language === "HINDI" ? "HI" : "EN"}
+            </Box>
+            <ArrowDropDown />
+          </FlexBox>
 
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '100%'
-        }}>
-            <FlexBox sx={{
-                height: '3rem',
-                justifyContent: 'space-between',
-                backgroundColor: 'var(--color-primary)',
-                padding: { md: '1rem 2rem', sm: '1rem', xs: '1rem' },
-            }}>
+          <PhoneOutlined
+            sx={{
+              cursor: "pointer",
+              display: { md: "flex", sm: "none", xs: "none" },
+            }}
+          />
 
-                <FlexBox sx={{ width: 'fit-content', gap: '2rem' }}>
-                    <img
-                        style={{
-                            height: '2rem',
-                            width: '2rem',
-                            cursor: 'pointer'
-                        }}
-                        src="https://acharyaprashant.org/images/ic_favicon.png"
-                        alt="Acharya Prashant" />
-                    <FlexBox sx={{
-                        gap: { md: '2rem', sm: '1rem' },
-                        display: { md: 'flex', sm: 'flex', xs: 'none' }
-                    }} >
-                        {navMenuList.map((item, idx) => (
-                            <FlexBox
-                                onClick={() => {
-                                    // item?.subMenu ? setShowSubMenu(true) : navigate(item.link)
-                                }}
-                                sx={{
-                                    width: 'fit-content',
-                                    color: 'white',
-                                    height: '100%',
-                                    gap: 0,
-                                    cursor: 'pointer'
-                                }} key={idx}>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              cursor: "pointer",
+            }}
+          >
+            <MenuOutlined />
+            <Typography
+              fontSize={"0.75rem"}
+              ml={"0.125rem"}
+              sx={{
+                display: { md: "block", sm: "block", xs: "none" },
+              }}
+            >
+              Menu
+            </Typography>
+          </Box>
+        </FlexBox>
+      </FlexBox>
 
-
-                                <Typography sx={{ fontSize: '0.875rem', textWrap: 'nowrap' }}>{item.title}</Typography>
-                                {item?.subMenu ? <ArrowDropDown /> : <></>}
-
-                                {item?.subMenu && showSubMenu && <SubMenu />}
-                            </FlexBox>
-                        ))}
-                    </FlexBox>
-                </FlexBox>
-
-                <FlexBox sx={{ width: 'fit-content', color: 'white', gap: '0.75rem' }}>
-
-                    <FlexBox gap={0} sx={{ cursor: 'pointer' }}
-                        onClick={switchLanguage}
-                    >
-                        <Box sx={{
-                            color: 'white',
-                            fontWeight: '600',
-                            border: '2px solid white',
-                            borderRadius: '0.25rem',
-                            fontSize: '0.8rem',
-                            paddingX: '0.5rem',
-                            display: 'flex',
-                            alignItems: 'center',
-                        }}>
-                            {language === 'HINDI' ? 'HI' : 'EN'}
-                        </Box>
-                        <ArrowDropDown />
-                    </FlexBox>
-
-
-                    <PhoneOutlined sx={{
-                        cursor: 'pointer',
-                        display: { md: 'flex', sm: 'none', xs: 'none' }
-
-                    }} />
-
-                    <Box sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        cursor: 'pointer'
-                    }}>
-                        <MenuOutlined />
-                        <Typography fontSize={'0.75rem'} ml={'0.125rem'}>Menu</Typography>
-                    </Box>
-
-
-                </FlexBox>
-
-            </FlexBox>
-
-            <FlexBox sx={{
-                backgroundColor: 'var(--color-bg-2)',
-                color: 'white',
-                padding: '.875rem 1rem',
-                justifyContent: { md: 'center', sm: 'space-between', xs: 'space-between' }
-            }}>
-
-                <Box sx={{ gap: '1rem', display: 'flex' }}>
-                    <Box sx={{
-                        backgroundColor: 'rgb(220 38 38)',
-                        width: '16px',
-                        marginTop: '0.25rem',
-                        height: '16px',
-                        borderRadius: '5rem',
-                        border: '1px solid white',
-                        marginRight: '-0.5rem',
-                        flexShrink: 0,
-                    }}>
-                    </Box>
-                    <Typography sx={{
-                        fontSize: { md: '1.125rem', sm: '1rem', xs: '1rem' }
-                    }}>{langStr.header_notice.text}</Typography>
-                </Box>
-
-
-                <a target='_blank' href='https://acharyaprashant.org/hi/contribute/contribute-work?cmId=m00071'>
-                    <Typography
-                        sx={{
-                            border: '1px solid white',
-                            borderRadius: '0.25rem',
-                            padding: '0.125rem 0.5rem',
-                            fontSize: '0.875rem',
-                            width: 'max-content',
-                            textWrap: 'nowrap',
-                            cursor: 'pointer',
-                            color: 'white'
-                        }}>{langStr.header_notice.button_text}</Typography>
-                </a>
-            </FlexBox>
+      <FlexBox
+        sx={{
+          backgroundColor: "var(--color-bg-2)",
+          color: "white",
+          padding: ".875rem 1rem",
+          justifyContent: {
+            md: "center",
+            sm: "space-between",
+            xs: "space-between",
+          },
+        }}
+      >
+        <Box sx={{ gap: "1rem", display: "flex" }}>
+          <Box
+            sx={{
+              backgroundColor: "rgb(220 38 38)",
+              width: "16px",
+              marginTop: "0.25rem",
+              height: "16px",
+              borderRadius: "5rem",
+              border: "1px solid white",
+              marginRight: "-0.5rem",
+              flexShrink: 0,
+            }}
+          ></Box>
+          <Typography
+            sx={{
+              fontSize: { md: "1.125rem", sm: "1rem", xs: "1rem" },
+            }}
+          >
+            {langStr.header_notice.text}
+          </Typography>
         </Box>
-    )
-}
 
-export default Header
+        <a
+          target="_blank"
+          href="https://acharyaprashant.org/hi/contribute/contribute-work?cmId=m00071"
+        >
+          <Typography
+            sx={{
+              border: "1px solid white",
+              borderRadius: "0.25rem",
+              padding: "0.125rem 0.5rem",
+              fontSize: "0.875rem",
+              width: "max-content",
+              textWrap: "nowrap",
+              cursor: "pointer",
+              color: "white",
+            }}
+          >
+            {langStr.header_notice.button_text}
+          </Typography>
+        </a>
+      </FlexBox>
+    </Box>
+  );
+};
+
+export default Header;
 
 const SubMenu = () => {
-    return (
-        <>
-            <p>Menu</p>
-        </>
-    )
-}
+  return (
+    <>
+      <p>Menu</p>
+    </>
+  );
+};
